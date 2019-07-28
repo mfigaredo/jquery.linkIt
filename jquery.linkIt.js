47aed778b95155207eefe2e4dc12e3a4d71d94a2
Miguel Figaredo <mfigaredo@gmail.com>
2019-07-28 13:49:32 -0500

    first release

--- jquery.linkIt.js
+++ jquery.linkIt.js
@@ -1,6 +1,51 @@
 /**
+ * name: LinkIt
+ * author: Miguel Figaredo
+ * version: 1.0.0
+ * license: MIT
+ * 
  * JQuery LinkIt Plugin
- * Version 1.0.0
+ * 
  */
+
+ (function($){
+    $.fn.linkIt = function(options) {
+        // alert(this.html());
+        // Default settings
+        var settings = $.extend({
+            href : null,
+            text: null,
+            target: '_self',
+            color: 'navy',
+            background: '',
+            decoration: 'none'
+        }, options);
+
+        // alert(settings.whatever);
+        // Validate
+        if(settings.href === null) {
+            console.log('You need href option for linkIt to work');
+            return this;
+        }
+
+        return this.each(function(){
+            var object = $(this);
+            if(settings.text == null) {
+                settings.text = object.text();
+
+            }
+            object.wrap('<a target="'+settings.target+'" href="'+settings.href+'"></a>')
+                .text(settings.text)
+                .css({
+                    'color' : settings.color,
+                    'background-color' : settings.background,
+                    'text-decoration': settings.decoration,
+                });
+        });
+
+
+    }
+
+ }(jQuery));
 
  
